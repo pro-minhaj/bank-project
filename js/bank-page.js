88a71773708b617535eb0ee8a36d287ccff22cc6
Md Minhaj <parsonal322532@gmail.com>
2023-11-21 00:23:02 +0600

    bank Project

---
+++ js/bank-page.js
@@ -0,0 +1,63 @@
+document.getElementById('logout-btn').addEventListener('click', function(){
+    window.location.href = 'login.html';
+});
+
+document.getElementById('deposit-btn').addEventListener('click', function(){
+    // Deposit Area
+    const depositFuild = document.getElementById('deposit-fuild');
+    const depositFuildString = depositFuild.value;
+    const depositFuildValue = parseFloat(depositFuildString);
+    depositFuild.value = '';
+
+    if(isNaN(depositFuildString)){
+        alert('Only Number Type')
+        return;
+    }
+    
+    const depositTotal = document.getElementById('deposit-total');
+    const  depositTotalString = depositTotal.innerText;
+    const depositTotalValue = parseFloat(depositTotalString);
+    
+    depositTotal.innerText = depositTotalValue + depositFuildValue;
+
+    // Balance Total
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalString = balanceTotal.innerText;
+    const balanceTotalValue= parseFloat(balanceTotalString);
+
+    balanceTotal.innerText = balanceTotalValue + depositFuildValue;
+});
+
+    // Withdraw Area
+
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+    const withdrawFuild = document.getElementById('withdraw-fuild');
+    const withdrawFuildString = withdrawFuild.value;
+    const withdrawFuildValue = parseFloat(withdrawFuildString);
+
+    withdrawFuild.value = '';
+
+    if(isNaN(withdrawFuildString)){
+        alert('Only Number Type');
+        return;
+    }
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalString = withdrawTotal.innerText;
+    const withdrawTotalValue = parseFloat(withdrawTotalString);
+
+
+
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalString = balanceTotal.innerText;
+    const balanceTotalValue= parseFloat(balanceTotalString);
+
+    if(withdrawFuildValue > balanceTotalValue){
+        alert("You Don't Have Enough Money")
+        return;
+    }
+
+    withdrawTotal.innerText = withdrawTotalValue + withdrawFuildValue;
+
+    balanceTotal.innerText = balanceTotalValue - withdrawFuildValue;
+})
---
+++ js/login.js
@@ -0,0 +1,19 @@
+document.getElementById('submit-btn').addEventListener('click', function(){
+        // Email Fuild
+    const emailFuild = document.getElementById('email-fuild');
+    const emailValue = emailFuild.value;
+
+        // Password Fuild
+    const passwordFuild = document.getElementById('password-fuild');
+    const passwordValue = passwordFuild.value;
+    
+    // Condetions
+    if(emailValue === 'minhaj@gmail.com' && passwordValue === '322532'){
+        window.location.href = 'bank-page.html';
+    }
+    else{
+        alert('Plase Enter Your Valid Email And Password');
+        emailFuild.value = '';
+        passwordFuild.value = '';
+    }
+})
